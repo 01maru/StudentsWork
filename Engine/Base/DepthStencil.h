7f20dd3858f3b17d9bbbb659022e067f48d1025c
YukiMaruichi <k021g1419@g.neec.ac.jp>
2023-06-21 16:52:55 +0900

    stencil commentout

--- Engine/Base/DepthStencil.h
+++ Engine/Base/DepthStencil.h
@@ -13,16 +13,18 @@ private:
 	ComPtr<ID3D12Resource> depthBuff_;
 	ComPtr<ID3D12DescriptorHeap> dsvHeap_;
 
-	D3D12_DEPTH_STENCIL_DESC depthStencilDesc = {};
+	//D3D12_DEPTH_STENCIL_DESC depthStencilDesc = {};
 public:
 	void Initialize(int32_t width, int32_t height, DXGI_FORMAT format);
 	void OMSetRenderTargets(D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle);
 	void ClearDepthStencilView(D3D12_CLEAR_FLAGS flags);
 
-	void SetDepthStencilDesc(D3D12_DEPTH_STENCIL_DESC desc) { depthStencilDesc = desc; }
-
+	//	Getter
 	DXGI_FORMAT GetDSVFormat() { return depthBuff_->GetDesc().Format; }
-	D3D12_DEPTH_STENCIL_DESC GetDSDesc() { return depthStencilDesc; }
+	//D3D12_DEPTH_STENCIL_DESC GetDSDesc() { return depthStencilDesc; }
 	ID3D12DescriptorHeap* GetDSVHeap() { return dsvHeap_.Get(); }
+
+	//	Setter
+	//void SetDepthStencilDesc(D3D12_DEPTH_STENCIL_DESC desc) { depthStencilDesc = desc; }
 };
 
